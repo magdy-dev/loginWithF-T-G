d54ca28307a89d4e26f61e0571e943426eea440a
mady shenoda <markobosh5@gmail.com>
2022-03-30 10:35:30 +0300

    login

---
+++ src/main/java/com/magdy/login_with_ftg/LoginWithFTGApplication.java
@@ -0,0 +1,13 @@
+package com.magdy.login_with_ftg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWithFTGApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginWithFTGApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/magdy/login_with_ftg/LoginWithFTGApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magdy.login_with_ftg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginWithFTGApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
